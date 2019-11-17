ba375875fc41ea0f024c7de97061d07c2eda19f9
John Cokos <highlander.44@gmail.com>
2019-11-17 20:27:18 +0200

    Class 06 Demo and Drawings

--- class-06/demos/api-demo/server.js
+++ class-06/demos/api-demo/server.js
@@ -1,34 +1,65 @@
 'use strict';
-// npm packages - 3rd party
-require('dotenv').config()
+
+// DOTENV (Read our Environment Variables) -- UpperCase
+require('dotenv').config();
+
+// Express Server
+// Express does all the headers (envelope stuff)
 const express = require('express');
+
+// CORS = Cross Origin Resource Sharing
 const cors = require('cors');
 
-// application constant
-const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
-app.get('/cool', (request, response) => {
-  response.send('cool data from the /cool route');
+const server = express();
+
+server.use( cors() );
+
+// Home Route (http://localhost:3000/)
+server.get('/', (request, response) => {
+  console.log(request);
+  response.status(200).send('You did a great job');
 });
 
-
-// http://localhost:3001/location?data=seattle
-app.get('/location', (request, response) => {
-  // send the users current location back to them
-  const geoData = require('./data/geo.json');
-  const city = request.query.data;
-  const locationData = new Location(city, geoData);
-  response.send(locationData);
+/*
+Object should look like this:
+{
+  "search_query": "seattle",
+  "formatted_query": "Seattle, WA, USA",
+  "latitude": "47.606210",
+  "longitude": "-122.332071"
+}
+ */
+server.get('/location', (request, response) => {
+  const locationData = require('./data/geo.json');
+  const location = new Location(locationData);
+  response.status(200).json(location);
 });
 
-function Location(city, geoData) {
-  this.search_query = city;
-  this.formatted_query = geoData.results[0].formatted_address;
-  this.latitude = geoData.results[0].geometry.location.lat;
-  this.longitude = geoData.results[0].geometry.location.lng;
+function Location( data ) {
+  this.search_query = 'lynnwood';
+  this.formatted_query = data.results[0].formatted_address;
+  this.latitude = data.results[0].geometry.location.lat;
+  this.longitude = data.results[0].geometry.location.lng;
 }
 
-app.listen(PORT, () => {
-  console.log(`listening on PORT ${PORT}`);
+// /weather route
+// Weather()
+
+// Force an Error to Happen (http://localhost:3000/boo)
+server.get('/boo', (request,response) => {
+  throw new Error('Whoops');
 });
+
+// When an error happens ...
+server.use('*', (request, response) =>{
+  response.status(404).send('Not Found');
+});
+
+server.use( (error, request, response) => {
+  response.status(500).send(error);
+});
+
+
+server.listen( PORT, () => console.log(`App listening on ${PORT}`));
