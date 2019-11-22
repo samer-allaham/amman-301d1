15e524d5ef5a14e6736e67072e800738abf3c2d3
John Cokos <highlander.44@gmail.com>
2019-11-22 10:19:01 +0200

    Demo code uploaded

--- class-08/demos/schema.sql
+++ class-08/demos/schema.sql
@@ -1,18 +1,7 @@
-DROP TABLE IF EXISTS weathers;
-DROP TABLE IF EXISTS locations;
+DROP TABLE IF EXISTS people;
 
-CREATE TABLE locations ( 
-    id SERIAL PRIMARY KEY, 
-    search_query VARCHAR(255), 
-    formatted_query VARCHAR(255), 
-    latitude NUMERIC(10, 7), 
-    longitude NUMERIC(10, 7)
-  );
-
-CREATE TABLE weathers ( 
-    id SERIAL PRIMARY KEY, 
-    forecast VARCHAR(255), 
-    time VARCHAR(255), 
-    location_id INTEGER NOT NULL,
-    FOREIGN KEY (location_id) REFERENCES locations (id)
-  );
+CREATE TABLE people (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255)
+);
